2f8edcc393859a7ef356d41f7c6d1253db1e7fe9
Vasile Gorcinschi <vasile.gorcinschi@improving.com>
2024-04-04 17:38:19 -0400

    Fixed run-gh-act script

--- run-gh-act.sh
+++ run-gh-act.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 DOCKER_IMAGE_NAME="my-act-image"
-ARC_PLATFORM="$(uname -m)"
+ARC_PLATFORM="linux/$(uname -m)"
 CONTAINER_ARCHITECTURE=""
+
+if [[ $ARC_PLATFORM == "linux/arm64" ]]; then
+  CONTAINER_ARCHITECTURE="--container-architecture=$ARC_PLATFORM"
+fi
 
 # Check if Docker image exists
 if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == "" ]]; then
   echo "Building Docker image..."
-  
   # Check if the machine architecture is not arm64
-  if [[ $ARC_PLATFORM == "arm64" ]]; then
-    ARC_PLATFORM="linux/arm64"
-    CONTAINER_ARCHITECTURE="--container-architecture $ARC_PLATFORM"
+  if [[ $ARC_PLATFORM == "linux/arm64" ]]; then
     docker buildx create --use
     docker buildx build --platform $ARC_PLATFORM -t $DOCKER_IMAGE_NAME -f Dockerfile.arm64 . --load
   else
