f1192bbbeed8e70050e3492f2b794c2b7eb041da
Vasile Gorcinschi <vasigorc@gmail.com>
2024-04-07 14:49:29 -0400

    Updated Cli command name

--- src/client/cli.rs
+++ src/client/cli.rs
@@ -5,7 +5,7 @@ use clap::{Args, Parser, Subcommand};
 #[derive(Parser)]
 #[command(about = "Accepts an array of Clock objects and a \
 max weight and returns best value for weight combinations for given clocks", long_about = None)]
-#[command(name = "Knapsack Solver")]
+#[command(name = "knapsack_problem")]
 #[command(version = "1.0")]
 pub struct Cli {
   #[command(subcommand)]
