94cb990962dc4fe0fe16a7f7e1e4a5b8bf42ebf9
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2024-04-07 15:54:59 -0400

    :sparkles: Added cli client support (#3)
    * :sparkles: Added cli client support
    
    * Simplified stack
    
    * Fixed run-gh-act script
    
    * :goal_net: Started using clap error handling
    
    * Updated Cli command name
    
    ---------
    
    Co-authored-by: Vasile Gorcinschi <vasile.gorcinschi@improving.com>

--- run-gh-act.sh
+++ run-gh-act.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 DOCKER_IMAGE_NAME="my-act-image"
-ARC_PLATFORM="$(uname -m)"
+ARC_PLATFORM="linux/$(uname -m)"
+CONTAINER_ARCHITECTURE=""
+
+if [[ $ARC_PLATFORM == "linux/arm64" ]]; then
+  CONTAINER_ARCHITECTURE="--container-architecture=$ARC_PLATFORM"
+fi
 
 # Check if Docker image exists
 if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == "" ]]; then
   echo "Building Docker image..."
-  
   # Check if the machine architecture is not arm64
-  if [[ $ARC_PLATFORM == "arm64" ]]; then
-    ARC_PLATFORM="linux/arm64"
+  if [[ $ARC_PLATFORM == "linux/arm64" ]]; then
     docker buildx create --use
     docker buildx build --platform $ARC_PLATFORM -t $DOCKER_IMAGE_NAME -f Dockerfile.arm64 . --load
   else
@@ -18,4 +21,4 @@ if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == "" ]]; then
 fi
 
 echo "Running gh act on $ARC_PLATFORM..."
-gh act --container-architecture $ARC_PLATFORM --pull=false --platform ubuntu-latest=$DOCKER_IMAGE_NAME -j build
+gh act "$CONTAINER_ARCHITECTURE" --pull=false --platform ubuntu-latest=$DOCKER_IMAGE_NAME -j build
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use clap::error::ErrorKind;
+use fs::read_to_string;
+use std::fs;
+
+use clap::{CommandFactory, Parser};
+use rust_decimal::Decimal;
+use serde_json::from_str;
+
+use knapsack_problem::client::cli::{Cli, Commands};
+use knapsack_problem::problem::Problem;
+use knapsack_problem::{Clock, GenericResult};
+
+fn main() -> GenericResult<()> {
+  let cli = Cli::parse();
+
+  let mut cmd = Cli::command();
+  match &cli.mode {
+    Commands::Cli(args) => {
+      let buffer = read_to_string(&args.clocks_file).unwrap_or_else(|error| {
+        cmd
+          .error(
+            ErrorKind::InvalidValue,
+            format!("Error opening file {}", error),
+          )
+          .exit();
+      });
+
+      let clocks: Vec<Clock> = from_str(&buffer).unwrap_or_else(|error| {
+        cmd
+          .error(
+            ErrorKind::ValueValidation,
+            format!("Failed to parse passed-in file {}", error),
+          )
+          .exit();
+      });
+
+      let weight = Decimal::from_f32_retain(args.weight)
+        .filter(|decimal| decimal.is_sign_positive())
+        .ok_or_else(|| format!("{} must be a positive FLOAT number", &args.weight))?;
+      let problem = Problem::new(&clocks, weight);
+      if let Some(result) = problem.get_best_knapsack() {
+        println!(
+          "The best Knapsack combination for the provided input is {:?}",
+          result
+        );
+      }
+    }
+    Commands::Tui => cmd
+      .error(ErrorKind::InvalidSubcommand, "Tui is not implemented yet")
+      .exit(),
+  }
+  Ok(())
 }
